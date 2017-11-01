340e075267f6906c228abe71e06da3a6aa3dabf9
David Kuruvilla <dapawn@gmail.com>
2017-11-01 14:18:14 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  attr_accessor :name, :balance, :status
+
+  def deposit(sum)
+    @balance += sum
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
+  
 end
