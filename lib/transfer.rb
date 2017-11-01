d23d5b317d78fd6476b1c2e9c8f4949c560c8b3d
David Kuruvilla <dapawn@gmail.com>
2017-11-01 15:15:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,13 +10,13 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
   def valid?
-    @sender.valid? && @receiver.valid?
+    @sender.balance - @amount > 0 && @receiver.valid?
   end
 
   def execute_transaction
     if valid? && @status == "pending"
       ((@sender.balance -= @amount) && (@receiver.balance += @amount))
-      @status = "executed"
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
